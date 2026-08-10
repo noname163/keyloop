39a32f8981147985a03414858b6ff26675219167
noname163 <danghuudat163@gmail.com>
2026-08-10 19:46:37 +0700

    initial project

---
+++ src/test/java/com/keyloop/unifieddocumentviewer/UnifieddocumentviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyloop.unifieddocumentviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifieddocumentviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
